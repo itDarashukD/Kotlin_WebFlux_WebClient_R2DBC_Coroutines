ee0cb48dc4aadaa7c730e2e7eb749514c72a5521
itDarashukD <itdarashukd@gmail.com>
2024-02-20 10:35:26 +0100

    1. cahge url for using R2DBS in app.yml 2. change repo to CoroutineCrudRepository

--- src/main/java/com/epam/mentoring/kotlin/repository/DogBreedRepository.java
+++ src/main/java/com/epam/mentoring/kotlin/repository/DogBreedRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.kotlin.CoroutineCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DogBreedRepository extends CrudRepository<DogBreed, Long> {
-//public interface DogBreedRepository extends CoroutineCrudRepository<DogBreed, Long> {
+//public interface DogBreedRepository extends CrudRepository<DogBreed, Long> {
+public interface DogBreedRepository extends CoroutineCrudRepository<DogBreed, Long> {
 
 }
